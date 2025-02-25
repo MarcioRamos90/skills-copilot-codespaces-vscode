b5baa551b20bbf859ce2cc088245e78f74b0d80b
Márcio Ramos <marcioalvesramos90@hotmail.com>
2025-02-25 17:01:18 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,20 +1,40 @@
 // Create a web server
-// Listen for requests on port 3000
-// Respond with a list of comments
 
+// Load the http module to create an http server.
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
+var path = require('path');
 
-var comments = ['Comment 1', 'Comment 2', 'Comment 3'];
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  var uri = url.parse(request.url).pathname;
+  var filename = path.join(process.cwd(), uri);
+  console.log(uri);
+  console.log(filename);
+  fs.exists(filename, function (exists) {
+    if (!exists) {
+      response.writeHead(404, { 'Content-Type': 'text/plain' });
+      response.write('404 Not Found\n');
+      response.end();
+      return;
+    }
 
-var server = http.createServer(function (req, res) {
-  console.log('Request received');
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, 'binary', function (err, file) {
+      if (err) {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.write(err + '\n');
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, 'binary');
+      response.end();
+    });
   });
-  res.end(JSON.stringify(comments));
 });
 
-server.listen(3000, function () {
-  console.log('Server listening on port 3000');
-});
+// Listen on port 8000, IP defaults to
