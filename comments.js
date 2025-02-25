bf452251426c02ec1c1abf38ad8d1e0b48f830e4
Márcio Ramos <marcioalvesramos90@hotmail.com>
2025-02-25 17:32:41 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,40 +1,16 @@
-// Create a web server
-
-// Load the http module to create an http server.
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var path = require('path');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  var uri = url.parse(request.url).pathname;
-  var filename = path.join(process.cwd(), uri);
-  console.log(uri);
-  console.log(filename);
-  fs.exists(filename, function (exists) {
-    if (!exists) {
-      response.writeHead(404, { 'Content-Type': 'text/plain' });
-      response.write('404 Not Found\n');
-      response.end();
-      return;
-    }
-
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, 'binary', function (err, file) {
-      if (err) {
-        response.writeHead(500, { 'Content-Type': 'text/plain' });
-        response.write(err + '\n');
-        response.end();
-        return;
-      }
-
-      response.writeHead(200);
-      response.write(file, 'binary');
-      response.end();
-    });
-  });
+// Create web server
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
 });
-
-// Listen on port 8000, IP defaults to
+app.get('/comments', function(request, response) {
+  response.sendFile(__dirname + '/views/comments.html');
+});
+app.get('/comments/:id', function(request, response) {
+  response.sendFile(__dirname + '/views/comment.html');
+});
+app.listen(3000, function() {
+  console.log('Web server started on port 3000');
+});
